078b68ac4320094e3e67d1d24cfb23eec8d8b55e
iamgauravkhare <iamgauravkha@gmail.com>
2024-02-21 20:12:03 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,7 +8,7 @@ export default defineConfig({
   },
   define: {
     global: {},
-    BACKEND_URL: process.env.VITE_API_URL,
-    RAJORPAY: process.env.VITE_RAJORPAY,
+    BACKEND_URL: JSON.stringify(process.env.VITE_API_URL),
+    RAJORPAY: JSON.stringify(process.env.VITE_RAJORPAY),
   },
 });
